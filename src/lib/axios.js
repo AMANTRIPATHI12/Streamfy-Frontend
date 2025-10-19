5759ace24e5d8e2580e894cd96619ee5079c5ccd
AMANTRIPATHI12 <amantripathi2244@gmail.com>
2025-10-19 16:02:38 +0530

    updating api

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const Base_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5501/api"
-    : "https://streamfy-backend.onrender.com/api"; // ✅ your Render backend URL
+    : "https://streamfy-backend.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: Base_URL,
-  withCredentials: true,
+  withCredentials: true, // needed for cookies/auth
 });
